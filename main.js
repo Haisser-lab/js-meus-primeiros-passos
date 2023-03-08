58f208e8a0fec397957860a91ef4abe3b54d361e
Haisser-lab <84329660+Haisser-lab@users.noreply.github.com>
2023-03-08 12:16:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let iceCream = 'strawberry';
+if (iceCream === 'chocolate') {
+    alert('Yay, I love chocolatecice cream');
+} else {
+    alert('Awww, but chocolate is my favorite...');
+}
